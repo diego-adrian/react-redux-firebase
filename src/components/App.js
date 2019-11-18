9009084ea0f7a16f918dc429f01da4bb6b4c6b6e
Adrian <dbarra@agetic.gob.bo>
2019-11-18 12:04:17 -0400

    container

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Grid columns="equal" className="app" style={{ background: '#EEE'}}>
+      <ColorPanel/>
+      <SidePanel/>
+      <Grid.Column style={{ marginLeft: 320}}>
+        <Messages/>
+      </Grid.Column>
+      <Grid.Column style={{ width: 4}}>
+        <MetaPanel/>
+      </Grid.Column>
+    </Grid>
   );
 }
 
